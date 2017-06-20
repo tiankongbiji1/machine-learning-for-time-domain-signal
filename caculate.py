6aa7d2224de2afb386197b3e9bff6680551e620b
tiankongbiji1 <945594385@qq.com>
2017-06-20 10:23:12 +0800

    Update caculate.py

--- caculate.py
+++ caculate.py
@@ -9,7 +9,6 @@ sess=tf.InteractiveSession()
 def weight_variable(shape):
  initial=tf.truncated_normal(shape,stddev=0.1)
  return tf.Variable(initial)
-
 def bias_variable(shape):
  initial=tf.constant(0.1,shape=shape)
  return tf.Variable(initial)
@@ -18,6 +17,7 @@ def bias_variable(shape):
  def conv2d(x,w):
    return tf.nn.conv2d(x,w,strides=[1,1,1,1],padding='SAME')
  #pooling
+
  def max_pool_2*2(x):
    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
    
@@ -47,4 +47,30 @@ def bias_variable(shape):
  h_pool2=max_pool_1*4(h_conv2)
  
  #define the connected layer
+ w_fc1=weight_variable([7*7*64,1024])
+ b_fc1=bias_variable([1024])
  
+ h_pool2_flat=tf.reshape(h_pool2,[-1,7*7*64])
+ h_fc1=tf.nn.relu(tf.matmul(h_pool2_flat,w_fc1)+b_fc1)
+ 
+ keep_prob=tf.placeholder("float")
+ h_fc1=tf.nn.dropout(h_fc1,keep_prob)
+ 
+w_fc2=weight_variable([1024,10])
+b_fc2=bias_variable([10])
+
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop,w_fc2)+b_fc2)
+
+cross_entrop=-tf.reduce_sum(y_*tf.log(y_conv))
+train_step=tf.train.GradientDescentOptimize(e-3).minimize(cross_entropy)
+
+correct_prediction=tf.equal)tf.argmax(y_conv,1),tf.argmax(y_,1))
+accuray=tf.reduce_mean(tf.cast(correct_prediction,"float"))
+sess.run(tf.initialize_all_variables())
+for i in range():
+ batch=mnist.train.next_batch(50)
+ if i%100==0:
+  train_accuracy=accuracy.eval(feed_dict={x:batch[0],y:batch[1],keep_prob:1.0})
+  print "step %d,train accuracy %g" %(i, train_accuracy)
+ train_step.run(feed_dict=[x:batch[0],y:batch[1],keep_prob:0.5})
+print "test accuracy %g" %accuracy.eval(feed_dict={x:mnist.test.images,y_:mnist.test.labels,keep_prob:1.0})
